be11929ea63ae11d6bf135172bd8d631ce08b47e
Jose-Prince <joseprince148@gmail.com>
2025-02-24 16:17:18 -0600

    build: new dependencies for dotnet project

--- flake.nix
+++ flake.nix
@@ -14,6 +14,17 @@
         buildInputs = [
             pkgs.dotnet-sdk_8
         ];
+        
+        shellHook = ''
+            echo "Assure Neo4J Driver is installed"
+            if [ ! -f "packages.lock.json" ]; then 
+                dotnet new console --force
+                dotnet add package Neo4J.Driver
+                dotnet add package Microsoft.AspNetCore.App
+                dotnet add package DotNetEnv
+                dotnet add package Swashbuckle.AspNetCore
+            fi
+        '';
     };
   };
 }
